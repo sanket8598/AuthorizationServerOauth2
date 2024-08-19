1c1051785a927db60de31646d7138418635ca0ad
Sanket Wakankar <Sanket Wakankar@ho.rnt.ai>
2024-08-19 19:22:24 +0530

    first commit

---
+++ src/main/java/ai/rnt/auth2/server/RntAuthorizationServerApplication.java
@@ -0,0 +1,13 @@
+package ai.rnt.auth2.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RntAuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RntAuthorizationServerApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ai/rnt/auth2/server/config/DefaultSecurityConfig.java
@@ -0,0 +1,43 @@
+package ai.rnt.auth2.server.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import ai.rnt.auth2.server.service.CustomAuthenticationProvider;
+
+import javax.sql.DataSource;
+
+@EnableWebSecurity
+public class DefaultSecurityConfig {
+
+    @Autowired
+    private CustomAuthenticationProvider customAuthenticationProvider;
+
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests ->
+                        authorizeRequests.anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Autowired
+    public void bindAuthenticationProvider(AuthenticationManagerBuilder authenticationManagerBuilder) {
+        authenticationManagerBuilder
+                .authenticationProvider(customAuthenticationProvider);
+    }
+}
